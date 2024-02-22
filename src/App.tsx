d275784c2a800a8120f6d768c524b795e1ebcd7f
김지원 <stayby16@naver.com>
2024-02-22 16:05:25 +0900

    🐛 first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CustomGlobe from "./CustomGlobe";
+
+function App() {
+  return (
+    <>
+      <CustomGlobe />
+    </>
+  );
+}
+
+export default App;
---
+++ src/CustomGlobe.tsx
@@ -0,0 +1,25 @@
+import Globe from "react-globe.gl"
+
+function CustomGlobe() {
+    const N = 20;
+    const arcsData = [...Array(N).keys()].map(() => ({
+      startLat: (Math.random() - 0.5) * 180,
+      startLng: (Math.random() - 0.5) * 100,
+      endLat: (Math.random() - 0.5) * 180,
+      endLng: (Math.random() - 0.5) * 500,
+      color: [['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)], ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]]
+    }));
+  return (
+    <Globe
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      arcsData={arcsData}
+      arcColor={'color'}
+      arcDashLength={() => Math.random()}
+      arcDashGap={() => Math.random()}
+      arcDashAnimateTime={() => Math.random() * 4000 + 500}
+      />
+  )
+}
+
+export default CustomGlobe
