69c84aee6dab5bef7e1696d58beef3e93754bde4
김지원 <stayby16@naver.com>
2024-02-23 09:24:42 +0900

    modifier #1

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,19 @@
+import Dropdown from "react-bootstrap/Dropdown";
 function Login() {
-  return <h1>Login Page</h1>;
-}
+  return (
+    <>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown Button
+        </Dropdown.Toggle>
 
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
+  );
+}
 export default Login;
