ce24c9f264fae7822319b686695570281186b213
김지원 <stayby16@naver.com>
2024-02-26 09:55:27 +0900

    ✨ tailwind css

--- src/component/Router.tsx
+++ src/component/Router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
-import Login from "./Login";
-import CustomGlobe from "./CustomGlobe";
-import ErrorComponent from "./error-component";
+import App from "../App";
+import Login from "../page/Login";
+import CustomGlobe from "../page/CustomGlobe";
+import ErrorComponent from "./error_component";
 
 const router = createBrowserRouter([
   {
